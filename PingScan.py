e55abef7dc4f683d654bafe763b75b4a319f0d88
jegasape <jegasape@gmail.com>
2018-12-01 00:20:46 -0400

    Tags

--- PingScan.py
+++ PingScan.py
@@ -2,11 +2,9 @@
 from subprocess import Popen, PIPE
 
 for ip in range(1,40):
-	ipAddress = '192.168.1.'+str(ip)
-	print "Scanning %s " %(ipAddress)
+	ipAddress = '10.0.0.'+str(ip)
+	print("Scanning %s " %(ipAddress))
 	subprocess = Popen(['/bin/ping', '-c 1 ', ipAddress], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 	stdout, stderr= subprocess.communicate(input=None)
-	if "bytes from " in stdout:
-		print "The Ip Address %s has responded with a ECHO_REPLY!" %(stdout.split()[1])
-		with open("ips.txt", "a") as myfile:
-			myfile.write(stdout.split()[1]+'\n')
+	if "bytes from " in str(stdout):
+		print("The Ip Address %s has responded with a ECHO_REPLY!" %(stdout.split()[1]))
